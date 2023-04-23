fdd20561f2237bac537206b9b1abdeda5b5564f8
Yahor Pronski <yahor_pronski@epam.com>
2023-04-23 15:43:21 +0300

    styles updates

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,6 +1,11 @@
+import { Link } from 'react-router-dom';
 import ImageText from "../ImageText";
 import '../../assets/styles/components/layout/footer.scss';
 import logo from '../../assets/images/logo.png';
+import facebook from '../../assets/images/social/facebook.png';
+import twitter from '../../assets/images/social/twitter.png';
+import instagram from '../../assets/images/social/instagram.png';
+import linkedin from '../../assets/images/social/linkedin.png';
 
 const Footer = () => {
     return (
@@ -9,13 +14,24 @@ const Footer = () => {
                 Sport Event Finder
             </ImageText>
             <div>
-                <p className="center">
+                <p>
                     © 2022 All rights reserved<br/>
                     <a href="#">Terms of use & privacy policy</a>
                 </p>
             </div>
-            <div>
-                Contacts
+            <div className="footer__contacts">
+                <Link to='/'>
+                    <img src={facebook} alt="facebook"/>
+                </Link>
+                <Link to='/'>
+                    <img src={twitter} alt="twitter"/>
+                </Link>
+                <Link to='/'>
+                    <img src={instagram} alt="instagram"/>
+                </Link>
+                <Link to='/'>
+                    <img src={linkedin} alt="linkedin"/>
+                </Link>
             </div>
         </footer>
     );
